a38a4bed65fc048d2f0a3d329e02e6cb3c95754b
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-31 22:31:42 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-
+import foods from './foods.json';
+import { FoodBox } from './components/FoodBox';
+import { AddFoodForm } from './components/AddFoodForm';
 
 function App() {
-  return 
+  return (
+    <div>
+        <AddFoodForm />
+
+        {foods.map((currentFood) => {
+          return (
+            <FoodBox
+              key={currentFood.name}
+              food={{
+                name: currentFood.name,
+                calories: currentFood.calories,
+                image: currentFood.image,
+                servings: currentFood.servings,
+              }}
+            />
+          );
+        })}
+      </div>
+  );
 }
 
 export default App;
